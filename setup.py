bd98e29c3c78894f8e3a0d74aa66f91280979bb0
Andreas Stuhlmueller <andreas@stuhlmueller.org>
2013-09-17 14:12:19 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='i3',
+      version='0.1',
+      description='i3',
+      author='Andreas Stuhlmueller',
+      author_email='ast@mit.edu',
+      url='https://github.com/stuhlmueller/i3',
+      packages=['i3'],
+     )
