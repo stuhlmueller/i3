62e90b2ba27d09fc5bc1da7dd456209e2243176a
Andreas Stuhlmueller <andreas@stuhlmueller.org>
2013-09-17 13:59:41 -0700

    Use py.test.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,24 +5,18 @@ Tests for sampling and probability calculation utilities.
 """
 
 import math
-import unittest
+import numpy as np
 
 from i3 import utils
 
+def test_safelog_base():
+  """Test that safe_log does not differ from log for values > 0."""
+  nums = [1, 10, 100, 1000, 10000]
+  np.testing.assert_array_almost_equal(
+    [utils.safe_log(num) for num in nums],
+    [math.log(num) for num in nums])
 
-class TestSafeLog(unittest.TestCase):
-  """Test safe_log function."""
-
-  def test_log_standard(self):
-    """Test that safe_log does not differ from log for values > 0."""
-    for num in [1, 10, 100, 1000]:
-      self.assertAlmostEqual(utils.safe_log(num), math.log(num))
-
-  def test_log_extended(self):
-    """Test that safe_log returns -inf for 0."""
-    self.assertEqual(utils.safe_log(0), utils.NEGATIVE_INFINITY)
-    self.assertEqual(utils.NEGATIVE_INFINITY, float("-inf"))
-
-
-if __name__ == '__main__':
-  unittest.main()
+def test_safelog_extended():
+  """Test that safe_log returns -inf for 0."""
+  assert utils.safe_log(0) == utils.NEGATIVE_INFINITY
+  assert utils.NEGATIVE_INFINITY == float("-inf")
