927b37b3344b982ecaadcdc46e7d62b38fe975eb
Andreas Stuhlmueller <andreas@stuhlmueller.org>
2013-09-17 14:22:53 -0700

    Added topological sorting.

---
+++ tests/test_toposort.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""
+Tests for topological sorting.
+"""
+
+from i3 import toposort
+
+def test_toposort():
+  """Test that sorted order is actually topological."""
+  edges = [(0, 1), (1, 2), (0, 2), (4, 3), (3, 0)]
+  ordered_nodes = toposort.toposort(edges)
+  assert ordered_nodes == [4, 3, 0, 1, 2]
