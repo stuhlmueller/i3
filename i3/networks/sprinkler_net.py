07627de60e3e981f3ddb7b442558971780a698fe
Andreas Stuhlmueller <andreas@stuhlmueller.org>
2013-09-18 15:42:30 -0700

    Add sprinkler network.

---
+++ i3/networks/sprinkler_net.py
@@ -0,0 +1,38 @@
+"""A discrete three-node network."""
+
+from i3 import bayesnet
+from i3 import distribution
+
+
+def get(rng):
+  rain_node = bayesnet.BayesNetNode(
+    name="Rain",
+    parents=[],
+    get_distribution=lambda: distribution.CategoricalDistribution(
+      [True, False], [.2, .8], rng))
+  sprinkler_node = bayesnet.BayesNetNode(
+    name="Sprinkler",
+    parents=[rain_node],
+    get_distribution=lambda rain: distribution.CategoricalDistribution(
+      [True, False],
+      [0.01, 0.99] if rain else [0.4, 0.6],
+      rng))
+  rain_node.add_child(sprinkler_node)
+  grass_cpt = {
+    (False, False): [0, 1],
+    (False, True): [0.8, 0.2],
+    (True, False): [0.9, 0.1],
+    (True, True): [0.99, 0.01]
+  }
+  grass_node = bayesnet.BayesNetNode(
+    name="Grass",
+    parents=[rain_node, sprinkler_node],
+    get_distribution=lambda rain, sprinkler: (
+      distribution.CategoricalDistribution(
+        [True, False],
+        grass_cpt[(rain, sprinkler)],
+        rng)))
+  sprinkler_node.add_child(grass_node)
+  rain_node.add_child(grass_node)
+  net = bayesnet.BayesNet([rain_node, sprinkler_node, grass_node])
+  return net
