7038dccb745c2fb1e62a34c4b84be2f3b39a372d
Andreas Stuhlmueller <andreas@stuhlmueller.org>
2013-10-24 20:28:52 -0700

    Added optional Gibbs precomputation to trainer.

--- i3/train.py
+++ i3/train.py
@@ -1,22 +1,36 @@
 from __future__ import division
 
+from i3 import gibbs
 from i3 import learn
 
 
 class Trainer(object):
   """Learn distributions for all conditionals in a BayesNetMap."""
 
-  def __init__(self, inverse_map):
-    """Extracting all distinct conditionals from inverse map."""
+  def __init__(self, net, inverse_map, precompute_gibbs):
+    """Extracting all distinct conditionals from inverse map.
+
+    Args:
+      net: a BayesNet
+      inverse_map: a BayesNetMap with inverses for the Bayes net
+      precompute_gibbs: a Boolean indicating whether to do exact
+        computation of Gibbs conditinoals during initialization.
+    """
+    self.net = net
     self.learners = {}
     self.inverse_map = inverse_map
-    for net in inverse_map.values():
-      for node in net.nodes_by_index:
+    for inverse_net in inverse_map.values():
+      for node in inverse_net.nodes_by_index:
         parents = node.parents
         parent_indices = tuple([parent.index for parent in parents])
         key = (parent_indices, node.index)
-        learner = self.learners.setdefault(
-          key, learn.CountLearner(node.support, net.rng))
+        if node.children or not precompute_gibbs:
+          learner = self.learners.setdefault(
+            key, learn.CountLearner(node.support, net.rng))
+        else:
+          forward_node = net.nodes_by_index[node.index]
+          learner = self.learners.setdefault(
+            key, learn.GibbsLearner(forward_node, net.rng))
         node.set_distribution(learner)
 
   def observe(self, world):
@@ -25,7 +39,7 @@ class Trainer(object):
       parent_values = [world.data[index] for index in parent_indices]
       node_value = world.data[node_index]
       learner.observe(parent_values, node_value)
-
+  
   def finalize(self):
     """Finalize learners, compile network."""
     for learner in self.learners.values():
