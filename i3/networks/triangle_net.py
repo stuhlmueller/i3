13967f18ff0c1773b4a789e64462f6f308e4f154
Andreas Stuhlmueller <andreas@stuhlmueller.org>
2013-10-24 21:34:43 -0700

    Adapt data path based on whether running locally or on cloud.

--- i3/networks/triangle_net.py
+++ i3/networks/triangle_net.py
@@ -1,13 +1,19 @@
+import cloud
 import os
 
 from i3 import uai_import
 
-data_path = os.path.join(os.path.dirname(__file__), "../../data/")
+
+def data_path():
+  if cloud.running_on_cloud():
+    return "/bucket/"
+  else:
+    return os.path.join(os.path.dirname(__file__), "../../data/")
 
 
 def get(rng, smooth=95):
   filename = os.path.join(
-    data_path, "networks/triangle-n120-s{}.uai".format(smooth))
+    data_path(), "networks/triangle-n120-s{}.uai".format(smooth))
   net = uai_import.load_network(filename, rng)
   return net
 
@@ -15,7 +21,7 @@ def get(rng, smooth=95):
 def evidence(index, smooth=95):
   assert index == 0
   filename = os.path.join(
-    data_path, "evidence/triangle-n120-s{}-1.evid".format(smooth))
+    data_path(), "evidence/triangle-n120-s{}-1.evid".format(smooth))
   evidence = uai_import.load_evidence(filename)
   assert len(evidence) == 1
   return evidence[0]
@@ -24,7 +30,7 @@ def evidence(index, smooth=95):
 def marginals(index, smooth=95):
   assert index == 0
   filename = os.path.join(
-    data_path, "marginals/triangle-n120-s{}-1.mar".format(smooth))
+    data_path(), "marginals/triangle-n120-s{}-1.mar".format(smooth))
   marginals = uai_import.load_marginals(filename)
   return marginals
 
