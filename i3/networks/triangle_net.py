cb61a6111ae4527c7b5f6220e8cf8ad7855b8bca
Andreas Stuhlmueller <andreas@stuhlmueller.org>
2013-10-24 20:29:10 -0700

    Wrap lines.

--- i3/networks/triangle_net.py
+++ i3/networks/triangle_net.py
@@ -5,23 +5,26 @@ from i3 import uai_import
 data_path = os.path.join(os.path.dirname(__file__), "../../data/")
 
 
-def get(rng):
-  filename = os.path.join(data_path, "networks/triangle-n120-s95.uai")
+def get(rng, smooth=95):
+  filename = os.path.join(
+    data_path, "networks/triangle-n120-s{}.uai".format(smooth))
   net = uai_import.load_network(filename, rng)
   return net
 
 
-def evidence(index):
+def evidence(index, smooth=95):
   assert index == 0
-  filename = os.path.join(data_path, "evidence/triangle-n120-s95-1.evid")
+  filename = os.path.join(
+    data_path, "evidence/triangle-n120-s{}-1.evid".format(smooth))
   evidence = uai_import.load_evidence(filename)
   assert len(evidence) == 1
   return evidence[0]
 
 
-def marginals(index):
+def marginals(index, smooth=95):
   assert index == 0
-  filename = os.path.join(data_path, "marginals/triangle-n120-s95-1.mar")
+  filename = os.path.join(
+    data_path, "marginals/triangle-n120-s{}-1.mar".format(smooth))
   marginals = uai_import.load_marginals(filename)
   return marginals
 
