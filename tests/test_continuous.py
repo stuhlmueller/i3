dd374cef8ad05ea3548d6c72d8775e35090fcd48
Andreas Stuhlmueller <andreas@stuhlmueller.org>
2013-11-02 20:08:41 -0700

    Clean up code style in test_continuous.

--- tests/test_continuous.py
+++ tests/test_continuous.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from i3 import bayesnet
 from i3 import dist
 from i3 import evid
@@ -8,26 +10,26 @@ from i3 import utils
 
 
 class TestBrightnessContrastBayesNet(object):
-  #     (with-query ((reflectance (gaussian 1 1)))
-  #         reflectance
-  #         (= (gaussian (* reflectance (+ (gamma 9 0.5) (gaussian 1 0.5))) 2) 10))
+  # (with-query ((reflectance (gaussian 1 1)))
+  #     reflectance
+  #     (= (gaussian (* reflectance (+ (gamma 9 0.5) (gaussian 1 0.5))) 2) 10))
 
   def setup(self):
     self.rng = utils.RandomState(seed=0)
     self.reflectance = bayesnet.DistRealBayesNetNode(
-        index=0, distribution=dist.GaussianDistribution(self.rng, 1, 1))
+      index=0, distribution=dist.GaussianDistribution(self.rng, 1, 1))
     self.illumination_1 = bayesnet.DistRealBayesNetNode(
-        index=1, distribution=dist.GammaDistribution(self.rng, 9, 0.5))
+      index=1, distribution=dist.GammaDistribution(self.rng, 9, 0.5))
     self.illumination_2 = bayesnet.DistRealBayesNetNode(
-        index=2, distribution=dist.GaussianDistribution(self.rng, 1, 0.5))
-    # self.illumination = bayesnet.DeterministicRealBayesNetNode(
-    #     index=3, function=lambda parents: parents[0] + parents[1],
-    #     rng=self.rng)
+      index=2, distribution=dist.GaussianDistribution(self.rng, 1, 0.5))
+    def observation_func(parents):
+      return dist.GaussianDistribution(
+        self.rng, parents[0] * (parents[1] + parents[2]), 2)
     self.observation = bayesnet.DistRealBayesNetNode(
-        index=3, distribution=dist.FunctionDistribution(self.rng,
-          lambda parents: dist.GaussianDistribution(self.rng, parents[0] * (parents[1] + parents[2]), 2)))
+      index=3, distribution=dist.FunctionDistribution(self.rng, observation_func))
     self.net = bayesnet.BayesNet(self.rng,
-        nodes=[self.reflectance, self.illumination_1, self.illumination_2, self.observation],
+        nodes=[self.reflectance, self.illumination_1,
+               self.illumination_2, self.observation],
         edges=[(self.reflectance, self.observation),
                (self.illumination_1, self.observation),
                (self.illumination_2, self.observation)])
@@ -44,14 +46,13 @@ class TestBrightnessContrastBayesNet(object):
       self.net, [self.observation], self.rng)
     print inverse_map.nets_by_key
     trainer = train.Trainer(self.net, inverse_map, False, k=50)
-    for _ in xrange(5000):
+    for _ in xrange(1000):
       world = self.net.sample()
       trainer.observe(world)
     trainer.finalize()
-
     evidence = evid.Evidence(keys=[self.observation], values=[5.5])
     proposal_size = 4
-    num_samples = 1000
+    num_samples = 500
     test_sampler = mcmc.InverseChain(
       self.net, inverse_map, self.rng, evidence, proposal_size)
     test_sampler.initialize_state()
@@ -61,9 +62,7 @@ class TestBrightnessContrastBayesNet(object):
       states.append(test_sampler.state)
       if i % 10 == 0:
         print states[-1]
-    def mean(items):
-      return sum(items) / float(len(items))
-    print mean([s[0] for s in states])
+    print np.mean([s[0] for s in states])
 
 
 if __name__ == '__main__':
